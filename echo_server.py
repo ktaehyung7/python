200e2cf3c4caac29098957f9b7bd5e873bc2336c
ktaehyung7 <55673916+ktaehyung7@users.noreply.github.com>
2021-11-16 17:58:56 +0900

    Add files via upload

---
+++ echo_server.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 16 13:49:23 2021
+
+@author: Raccoon
+"""
+
+import socket
+
+HOST='127.0.0.1'
+PORT=9999
+
+server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+server_socket.bind((HOST, PORT))
+
+server_socket.listen()
+
+client_socket, addr = server_socket.accept()
+
+print('Connected by', addr)
+
+while True:
+    data = client_socket.recv(1024)
+    if not data:
+        break
+    
+    print('Receive from', addr, data.decode())
+    client_socket.sendall(data)
+    
+client_socket.close()
+server_socket.close()
